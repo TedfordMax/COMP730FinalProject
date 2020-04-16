2f25450fe0226f874b520120efdee739091255ce
nn1043 <55171942+nn1043@users.noreply.github.com>
2020-04-16 17:11:19 -0400

    made variables static

--- Hero.java
+++ Hero.java
@@ -1,25 +1,21 @@
 public class Hero {
 
-    private String name = "None";
-    private int health = 0;
-    private int attack = 0;
-    private int speed = 0;
+    private static String name = "None";
+    private static int health = 0;
+    private static int attack = 0;
+    private static int speed = 0;
 
     public Hero(String n, int h, int a, int s) {
-        this.name = n;
-        this.health = h;
-        this.attack = a;
-        this.speed = s;
-
-        System.out.println(n);
-        System.out.println(name);
+        name = n;
+        health = h;
+        attack = a;
+        speed = s;
     }
 
     public static void ReadStats() {
-        System.out.println("I don't work. Beep boop.");
-        //System.out.println(name);
-        //System.out.println("Health: " + health);
-        //System.out.println("Attack: " + attack);
-        //System.out.println("Speed: " + speed);
+        System.out.println(name);
+        System.out.println("Health: " + health);
+        System.out.println("Attack: " + attack);
+        System.out.println("Speed: " + speed);
     }
 }
