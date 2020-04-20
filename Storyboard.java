5f5254c1dbbe639522de33e30ac8fb4929d06ca8
Scott <scg1009@wildcats.unh.edu>
2020-04-20 19:22:08 -0400

    Starting NEW combat system after this commit

--- Storyboard.java
+++ Storyboard.java
@@ -5,31 +5,26 @@ import java.util.Random;
 public class Storyboard extends CharacterCreator {
     public static void main(String[] args) {
         CharacterSelection();
-        // based on line 6, line 8 needs to have the variable that stores the character String.
-        // However, when I do 
-        // Chapter1(character), i get a "cannot find symbol" error.
-        // we need to remove any hardcoded character names
-        // -scott
     }
 
     public static void Story(String character) {
         Scanner sc = new Scanner(System.in);
         System.out.println("\n____________________\nCHAPTER 1\nSEARCHING FOR FOOD\n");
-        try{TimeUnit.SECONDS.sleep(3);}catch (InterruptedException e){e.printStackTrace();}
+        try{TimeUnit.SECONDS.sleep(1);}catch (InterruptedException e){e.printStackTrace();}
         System.out.println(character + ": There's no food left here...");
-        //try{TimeUnit.SECONDS.sleep(3);}catch (InterruptedException e){e.printStackTrace();}
+        try{TimeUnit.SECONDS.sleep(1);}catch (InterruptedException e){e.printStackTrace();}
         System.out.println(character + ": Ever since the Black Water appeared nearby, The Waterloon Reef has so many new sealife!");
         Crossroads(character, 1);
         System.out.println("Chapter 2");
-        
     }
+
     public static void Crossroads(String character, int chapter) {
         Scanner sc = new Scanner(System.in);
         switch(chapter) {
             case 1: // crossroads chapter 1
-                //try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                try{TimeUnit.SECONDS.sleep(1);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println(character + ": Should I swim down to find food, or swim to another reef?");
-                //try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("(1) If you swim down, you have to fight a swordfish.\n(2) If you swim to a nearby reef, you have to fight humans who are cleaning up an oil spill.");
                 int option = sc.nextInt();
                 switch(option) {
@@ -47,6 +42,7 @@ public class Storyboard extends CharacterCreator {
                 break;       
         }
     }
+
     public static void Chapter1A(String character) {
         Scanner sc = new Scanner(System.in);
         //try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
