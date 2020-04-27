555fca62e765e2abdcf5f29e0b91bf4e817b926b
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-27 18:57:06 -0400

    Update AssassinStarfish.java

--- AssassinStarfish.java
+++ AssassinStarfish.java
@@ -1,68 +1,38 @@
 
-public class AssassinStarfish implements CharacterInterface {
-    public int health = 4;
-    public int strength = 2;
-    public int sentience = 3;
-    public int attack = 1;
-    public int spell = 3;
-    public int xp = 0;
-    public void getStats() {
-        System.out.println("Health:    4\n");
-        System.out.println("Strength:  2\n");
-        System.out.println("Sentience: 3");
+public class AssassinStarfish extends Character {
+	
+	static public int Attack = 2;
+	static public int Sentience = 3;
+	static public int Health = 2;
+	static public String Attack1 = "Pinhead Slam";
+	static public String Attack2 = "The Real Dirty Dan";
+	static public String Attack3 = "Toxic Release";
+  
+    public static void getStats() {
+    	System.out.println("Attack = " + Attack);
+    	System.out.println("Sentience = " + Sentience);
+    	System.out.println("Health = " + Health);
     }
-    public void getOffense() {
-        System.out.println("(1) Attack: (-1) Starfish Slam");
-        System.out.println("(2)  Spell: (-3) The Dirty Dan");
-        System.out.println("(3) Defend:  (0) Camouflage");
+    public static void getOffense() {
+    	System.out.println("Attack 1 = " + Attack1);
+    	System.out.println("Attack 2 = " + Attack2);
+    	System.out.println("Attack 3 = " + Attack3);
     }
-    public int Attacking(int option) {
-        switch (option) {
-            case 1:
-                return -1;
-            case 2:
-                return -3;
-            case 3:
-                return 0;
-            default:
-                int error = 0;
-                return error;
-        }
+    public static void Attack1() {
     }
-    public String AttackingString(int option) {
-        switch (option) {
-            case 1:
-                String attack = "Crab Stab";
-                return attack;
-            case 2:
-                String spell = "Rangoon Confusion";
-                return spell;
-            case 3:
-                String defend = "Shell";
-                return defend;
-            default:
-                String error = "error";
-                return error;
-        }
-    }
-    public int Health(int change) {
-        health += change;
-        return health;
-    }
-    public int Strength(int change) {
-        strength += change;
-        return strength;
-    }
-    public int Attack(int change) {
-        attack += change;
-        return attack;
-    }
-    public int Spell(int change) {
-        spell += change;
-        return spell;
-    }
-    public int Sentience(int change) {
-        sentience += change;
-        return sentience;
-    }
+    public static void Attack2() {
+    	Attack = Attack + 3;
+    	
+	}
+	public static void Attack3() {
+		Attack = Attack*2;
+	}
+	public static void setVariables() {
+		Character.Attack = Attack;
+		Character.Sentience = Sentience;
+		Character.Health = Health;
+		Character.Attack1 = Attack1;
+		Character.Attack2 = Attack2;
+		Character.Attack3 = Attack3;
+	}
 }
