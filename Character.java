a76ece1392d0d596b2d4259103d49b086110fa5f
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-27 19:28:13 -0400

    hard coded the abilities into Attack2() and Attack(), added text when Attacks are made

--- Character.java
+++ Character.java
@@ -4,24 +4,60 @@ public class Character {
 	static public int Attack;
 	static public int Sentience;
 	static public int Health;
-	public String Attack1;
-	public String Attack2;
-	public String Attack3;
+	public static String Attack1;
+	public static String Attack2;
+	public static String Attack3;
+	public static String VictorySpeech;
+	public static String DefeatSpeech;
+	
 	    
-    public void getStats() {
+    public static void getStats() {
     	System.out.println("Attack = " + Attack);
     	System.out.println("Sentience = " + Sentience);
     	System.out.println("Toughness = " + Health);
     }
-    public void getOffense() {
+    public static void getOffense() {
     	System.out.println("Attack 1 = " + Attack1);
     	System.out.println("Attack 2 = " + Attack2);
     	System.out.println("Attack 3 = " + Attack3);
     }
-    public void Attack1() {
+    public static void Attack1() {
+    	System.out.println("You attack for" + Attack + " damage!");
     }
-    public void Attack2() {
+    public static void Attack2() {
+    	if (Attack2 == "Ink Squirt") {
+    		Attack = Attack + Sentience;
+    		System.out.println("You used Ink Squirt, your attack increased to = " + Attack);
+    	}
+    	if (Attack2 == "Crabtivating Wind-Up") {
+    		Attack = Attack*Sentience;
+    		System.out.println("You used Crabtivating Wind-Up, your attack increased to = " + Attack);
+    }
+    	if (Attack2 == "The Real Dirty Dan") {
+    		Attack = Attack + 3;
+    		System.out.println("You used The Real Dirty Dan, your attack increased to = " + Attack);
+    	}
 	}
-	public void Attack3() {
+	public static void Attack3() {
+		if (Attack3 == "Tentacle Block") {
+			Health = Health + Sentience*2;
+			System.out.println("You used Tentacle Block, your health increased to = " + Health);
+			}
+		if (Attack3 == "Toxic Release") {
+			Attack = Attack*2;
+			System.out.println("You used Toxic Release, your attack increased to = " + Attack);
+		}
+		if (Attack3 == "Krusty Exterior") {
+			Health = Health*Sentience;
+			System.out.println("You used Krusty Exterior, your health increased to = " + Health);
+		}
+	}
+	public void VictoryLine() {
+		System.out.println(VictorySpeech);
+	}
+	public void DefeatLine() {
+		System.out.println(DefeatSpeech);
+	}
+	public void takeDamage() {
 	}
 }
