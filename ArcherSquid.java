bdb808ed7e17993137321dc2878a4474bebf4f44
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-04-27 18:56:10 -0400

    Update ArcherSquid.java

--- ArcherSquid.java
+++ ArcherSquid.java
@@ -1,68 +1,40 @@
 
-public class ArcherSquid implements CharacterInterface {
-    public static int health = 4;
-    public int strength = 2;
-    public int sentience = 1;
-    public int attack = 2;
-    public int spell = 3;
-    public int xp = 0;
-    public void getStats() {
-        System.out.println("Health:    3\n");
-        System.out.println("Strength:  2\n");
-        System.out.println("Sentience: 1");
+public class ArcherSquid extends Character {
+	static public int Attack = 3;
+	static public int Sentience = 2;
+	static public int Health = 3;
+	static public String Attack1 = "Ink-tipped Torpedo";
+	public static String Attack2 = "Ink Squirt";
+	static public String Attack3 = "Tentacle Block";
+  
+    public static void getStats() {
+    	System.out.println("Attack = " + Attack);
+    	System.out.println("Sentience = " + Sentience);
+    	System.out.println("Health = " + Health);
     }
-    public void getOffense() {
-        System.out.println("(1) Attack: (-2) Ink-Tipped Torpedo");
-        System.out.println("(2)  Spell: (-3) Ink Cloud");
-        System.out.println("(3) Defend: (0) Tentacle Block");
+    public static void getOffense() {
+    	System.out.println("Attack 1 = " + Attack1 + " does damage based on attack.");
+    	System.out.println("Attack 2 = " + Attack2 + " increases health by current attack plus sentience");
+    	System.out.println("Attack 3 = " + Attack3 + " increases health by current health by sentience x 2");
     }
-    public int Attacking(int option) {
-        switch (option) {
-            case 1:
-                return -2;
-            case 2:
-                return -3;
-            case 3:
-                return 0;
-            default:
-                int error = 0;
-                return error;
-        }
+    public static void Attack1() {
+    	
     }
-    public String AttackingString(int option) {
-        switch (option) {
-            case 1:
-                String attack = "Ink-Tipped Torpedo";
-                return attack;
-            case 2:
-                String spell = "Ink Cloud";
-                return spell;
-            case 3:
-                String defend = "Tentacle Block";
-                return defend;
-            default:
-                String error = "error";
-                return error;
-        }
-    }
-    public int Health(int change) {
-        health += change;
-        return health;
-    }
-    public int Strength(int change) {
-        strength += change;
-        return strength;
-    }
-    public int Attack(int change) {
-        attack += change;
-        return attack;
-    }
-    public int Spell(int change) {
-        spell += change;
-        return spell;
-    }
-    public int Sentience(int change) {
-        sentience += change;
-        return sentience;
-    }
+    public static void Attack2() {
+    	Attack = Attack + Sentience;
+	}
+	public static void Attack3() {
+		Health = Health + Sentience*2;
+	}
+	public void takeDamage() {
+	}
+	public static void setVariables() {
+		Character.Attack = Attack;
+		Character.Sentience = Sentience;
+		Character.Health = Health;
+		Character.Attack1 = Attack1;
+		Character.Attack2 = Attack2;
+		Character.Attack3 = Attack3;
+		
+	}
 }
