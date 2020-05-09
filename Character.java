b91dc3a65a2d3ae749c6f9e30ed5773902863400
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-09 18:52:17 -0400

    Update Character.java

--- Character.java
+++ Character.java
@@ -1,7 +1,10 @@
 import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Character {
 	
+	static public Integer Level;
 	static public int Attack;
 	static public int Sentience;
 	static public int Health;
@@ -14,16 +17,22 @@ public class Character {
 	public static String WeaponName;
 	public static Integer WeaponStats;
 	public static Integer WeaponSentience;
+	public static String SecondaryWeaponName;
+	public static Integer SecondaryWeaponStats;
+	public static Integer SecondaryWeaponSentience;
+	public static Integer SecondaryWeaponHealth;
 	public static String ArmorName;
 	public static Integer ArmorStats;
-	public static Integer ArmorSentience;	
+	public static Integer ArmorSentience;
+	public static String WeaponType;
+	public static String SecondaryWeaponType;
 	    
     public static void getStats() {
     	System.out.println("Attack = " + Attack);
     	System.out.println("Sentience = " + Sentience);
     	System.out.println("Toughness = " + Health);
-    }
-    public static void getOffense() {
+	}
+	public static void getOffense() {
     	System.out.println("Attack 1 = " + Attack1);
     	System.out.println("Attack 2 = " + Attack2);
     	System.out.println("Attack 3 = " + Attack3);
@@ -65,24 +74,32 @@ public class Character {
 	public void DefeatLine() {
 		System.out.println(DefeatSpeech);
 	}
-	public static void WeaponItem(String newWeaponName, Integer newWeaponStats, Integer newWeaponSentience) {
+	public static void WeaponItem(String newWeaponName, String newWeaponType, Integer newWeaponStats, Integer newWeaponSentience) {
 		WeaponName = newWeaponName;
 		WeaponStats = newWeaponStats;
 		WeaponSentience = newWeaponSentience;
+		WeaponType = newWeaponType;
+		//WeaponInventory.add(WeaponName);
+	}
+	public static void SecondaryWeaponItem(String newSecondaryWeaponName, String newSecondaryWeaponType, Integer newSecondaryWeaponStats, Integer newSecondaryWeaponSentience, Integer newSecondaryWeaponHealth) {
+		SecondaryWeaponName = newSecondaryWeaponName;
+		SecondaryWeaponType = newSecondaryWeaponType;
+		SecondaryWeaponStats = newSecondaryWeaponStats;
+		SecondaryWeaponSentience = newSecondaryWeaponSentience;
+		SecondaryWeaponHealth = newSecondaryWeaponHealth;
 	}
 	public static void ArmorItem(String newArmorName, Integer newArmorStats, Integer newArmorSentience ){
 		ArmorName = newArmorName;
 		ArmorStats = newArmorStats;
 		ArmorSentience = newArmorSentience;
 	}
-	public void QuestItem(String newQuestItem) {
-		String questItem1 = " ";
-		String questItem2 = " ";
-		String questItem3 = " ";
-	}
+	//private static List<String> WeaponInventory(String WeaponName) {
+		//return WeaponInventory;
+		
+	//}
 	public static void getItemStats() {
-		Attack = Attack + WeaponStats;
-		Health = Health + ArmorStats;
+		Attack = Attack + WeaponStats + SecondaryWeaponStats;
+		Health = Health + ArmorStats + SecondaryWeaponHealth;
 		Sentience = Sentience + WeaponSentience + ArmorSentience;
 	}
 	public static void levelUp() {
@@ -90,8 +107,16 @@ public class Character {
 		Attack = Attack + 2;
 		Health = Health + 5;
 		Sentience = Sentience + 2;
+		Level = Level + 1;
+		if (XP >= 10) {
+			XP = XP - 10;
 		}
-	}	
+		System.out.println("You leveled up! Attack and Sentience increased by 2 and Health increased by 5!");
+		}
+	}
+	public static void checkLevel() {
+		System.out.println("You are level " + Level);
+	}
 	public static void checkItems() {
 		System.out.println("Weapon: " + WeaponName + " Increases Attack by: " + WeaponStats);
 		System.out.println("Armor: " + ArmorName + " Increses Health by: " + ArmorStats);
