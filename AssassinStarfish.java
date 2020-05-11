a19c3328bffd57370fd7d673184b75295f789feb
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-11 15:59:14 -0400

    Update AssassinStarfish.java

--- AssassinStarfish.java
+++ AssassinStarfish.java
@@ -1,9 +1,10 @@
 
 public class AssassinStarfish extends Character {
-	
-	static public int Attack = 2; 
+	static public Integer Level = 1;
+	static public int Attack = 3; 
 	static public int Sentience = 2;
-	static public int Health = 3;
+	static public int Health = 2;
+	static public int DefaultHealth = 2;
 	static public String Attack1 = "Ink-tipped Torpedo";
 	static public String Attack2 = "The Real Dirty Dan";
 	public static String Attack3 = "Toxic Release";
@@ -16,6 +17,12 @@ public class AssassinStarfish extends Character {
 	public static String ArmorName = "";
 	public static Integer ArmorStats = 0;
 	public static Integer ArmorSentience = 0;
+	public static String SecondaryWeaponName = "";
+	public static Integer SecondaryWeaponStats = 0;
+	public static Integer SecondaryWeaponSentience = 0;
+	public static Integer SecondaryWeaponHealth = 0;
+	public static String WeaponType = "";
+	public static String SecondaryWeaponType = "";
   
     public static void getStats() {
     	System.out.println("Attack = " + Attack);
@@ -37,9 +44,11 @@ public class AssassinStarfish extends Character {
 		Attack = Attack*2;
 	}
 	public static void setVariables() {
+		Character.Level = Level;
 		Character.Attack = Attack;
 		Character.Sentience = Sentience;
 		Character.Health = Health;
+		Character.DefaultHealth = DefaultHealth;
 		Character.Attack1 = Attack1;
 		Character.Attack2 = Attack2;
 		Character.Attack3 = Attack3;
@@ -52,5 +61,11 @@ public class AssassinStarfish extends Character {
 		Character.ArmorName = "";
 		Character.ArmorStats = 0;
 		Character.ArmorSentience = 0;
+		Character.SecondaryWeaponName = "";
+		Character.SecondaryWeaponHealth = 0;
+		Character.SecondaryWeaponStats = 0;
+		Character.SecondaryWeaponSentience = 0;
+		Character.WeaponType = "";
+		Character.SecondaryWeaponType = "";
 	}
 }
