f6b4d2a46577f213b18e9af791170d7773874db6
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-11 14:57:10 -0400

    Update Plankton.java
    Made into final boss

--- Plankton.java
+++ Plankton.java
@@ -1,34 +1,53 @@
 
-public class Plankton extends Character {
-    	static int Attack = 4;
-    	static int Sentience = 16;
-    	static int Health = 16;	
-    	Character user = new Character();
-    
-    public void getStats() {
-    	System.out.println("The Plankton has " + Attack + "attack.");
-    	System.out.println("The Plankton has " + Sentience + "sentience.");
-    	System.out.println("The Plankton has " + Health + "health.");
+public class Plankton extends Enemy {
+		static String Name = "The Final Boss";
+    	static int Attack = 10;
+    	static int Sentience = 8;
+    	static int Health = 30;	
+    	static String VictorySpeech = "The Final Boss: Goodbye everyone, I'll remember you all in therapy.";
+    	static String DefeatSpeech = "The Final Boss: That's it Mister! You just lost your brain privileges!";
+    	
+    public static void getStats() {
+    	System.out.println("The Final Boss has " + Attack + "attack.");
+    	System.out.println("The Final Boss has " + Sentience + "sentience.");
+    	System.out.println("The Final Boss has " + Health + "health.");
     }
     
-    public void getOffense() {
+    public static void getOffense() {
         System.out.println("(1) Attack: Deals " + Attack + " damage");
         System.out.println("(2)  Spell: Chummy Delight- Increases Plankton's attack by " + Sentience/2);
         System.out.println("(3) Defend: Reflect- Increases health by " + (Sentience/2));
     }
     
-    public void Attack1() {
-    	System.out.println("The Angler Fish attacks you, dealing " + Attack + " damage.");
+    public static void setStats() {
+    	Enemy.Name = Name;
+    	Enemy.Attack = Attack;
+    	Enemy.Sentience = Sentience;
+    	Enemy.Health = Health;
+    	Enemy.VictorySpeech = VictorySpeech;
+    	Enemy.DefeatSpeech = DefeatSpeech;
+    }
+    public static void Attack1() {
+    	System.out.println("The Final Boss attacks you, dealing " + Attack + " damage.");
     }	
     
-    public void Attack2() {
-    	Character.Attack = Character.Attack - 4;
-    	System.out.println("The Angler Fish increases the brightness of it's photophore, making it hard to see and decreasing your attack by " + 4);
+    public static void Attack2() {
+    	Character.Attack = Character.Attack - Enemy.Sentience;
+    	System.out.println("The Final Boss shoots freakin laser beams out of it's eyes, decreasing your attack by " + Enemy.Sentience);
     }
 
-    public void Attack3() {
+    public static void Attack3() {
     	Health = Health + Sentience/2;
-    	System.out.println("Plankton adds to his robotic suite, increasing his defense by  " + (Sentience/2));
+    	System.out.println("The Final Boss adds to his robotic suite, increasing his defense by  " + (Sentience/2));
     }
+    public static void VictoryLine() {
+		System.out.println(VictorySpeech);
+	}
+	public static void DefeatLine() {
+		if (Character.ArmorName == "Warrior Krab") {
+			System.out.println("You'll pay for this, Krabs!");
+		}
+		else System.out.println(DefeatSpeech);
+	}
 }
 
