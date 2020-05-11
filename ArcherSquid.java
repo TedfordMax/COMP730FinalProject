40abfd9561b3b4adf24b8f287e13d57cdd7cf949
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-11 15:58:58 -0400

    Update ArcherSquid.java

--- ArcherSquid.java
+++ ArcherSquid.java
@@ -1,73 +1,76 @@
 
 public class ArcherSquid extends Character {
 	
-    public int health = 3;
-    public int strength = 2;
-    public int sentience = 2;
-    public int attack = 3;
-    public int spell = 3;
-    public int xp = 0;
-    public void getStats() {
-        System.out.println("Health:    4\n");
-        System.out.println("Strength:  3\n");
-        System.out.println("Sentience: 2");
+	static public Integer Level = 1;
+	static public int Attack = 2;
+	static public int Sentience = 3;
+	static public int Health = 2;
+	static public int DefaultHealth = 2;
+	static public String Attack1 = "Ink-tipped Torpedo";
+	public static String Attack2 = "Ink Squirt";
+	static public String Attack3 = "Tentacle Block";
+	public static String VictorySpeech = "I win";
+	public static String DefeatSpeech = "I lose";
+	public static Integer XP = 0;
+	public static String WeaponName = "";
+	public static Integer WeaponStats = 0;
+	public static Integer WeaponSentience = 0;
+	public static String ArmorName = "";
+	public static Integer ArmorStats = 0;
+	public static Integer ArmorSentience = 0;
+	public static String SecondaryWeaponName = "";
+	public static Integer SecondaryWeaponStats = 0;
+	public static Integer SecondaryWeaponSentience = 0;
+	public static Integer SecondaryWeaponHealth = 0;
+	public static String WeaponType = "";
+	public static String SecondaryWeaponType = "";
+	    
+    public static void getStats() {
+    	System.out.println("Attack = " + Attack);
+    	System.out.println("Sentience = " + Sentience);
+    	System.out.println("Health = " + Health);
     }
-    public void getOffense() {
-        System.out.println("(1) Attack: (-1) Ink-tipped Torpedo");
-        System.out.println("(2)  Spell: (-2) Ink Squirt");
-        System.out.println("(3) Defend:  (0) Tentacle Block");
+    public static void getOffense() {
+    	System.out.println("Attack 1 = " + Attack1 + " does damage based on attack.");
+    	System.out.println("Attack 2 = " + Attack2 + " increases attack by current attack plus sentience");
+    	System.out.println("Attack 3 = " + Attack3 + " increases health by current health by sentience x 2");
     }
-    public int Attacking(int option) {
-        switch (option) {
-            case 1:
-                return -1;
-            case 2:
-                return -2;
-            case 3:
-                return 1;
-            default:
-                int error = 0;
-                return error;
-        }
+    public static void Attack1() {
+    	
     }
-    public String AttackingString(int option) {
-        switch (option) {
-            case 1:
-                String attack = "Crab Stab";
-                return attack;
-            case 2:
-                String spell = "Rangoon Confusion";
-                return spell;
-            case 3:
-                String defend = "Shell";
-                return defend;
-            default:
-                String error = "error";
-                return error;
-        }
-    }
-    public int XP(int change) {
-        xp += change;
-        return xp;
-    }
-    public int Health(int change) {
-        health += change;
-        return health;
-    }
-    public int Strength(int change) {
-        strength += change;
-        return strength;
-    }
-    public int Attack(int change) {
-        attack += change;
-        return attack;
-    }
-    public int Spell(int change) {
-        spell += change;
-        return spell;
-    }
-    public int Sentience(int change) {
-        sentience += change;
-        return sentience;
-    }
+    public static void Attack2() {
+    	Attack = Attack + Sentience;
+	}
+	public static void Attack3() {
+		Health = Health + Sentience*2;
+	}
+	public void takeDamage() {
+	}
+	public static void setVariables() {
+		
+		Character.Level = Level;
+		Character.Attack = Attack;
+		Character.Sentience = Sentience;
+		Character.Health = Health;
+		Character.DefaultHealth = DefaultHealth;
+		Character.Attack1 = Attack1;
+		Character.Attack2 = Attack2;
+		Character.Attack3 = Attack3;
+		Character.VictorySpeech = "I win";
+		Character.DefeatSpeech = "I lose";
+		Character.XP = 0;
+		Character.WeaponName = "";
+		Character.WeaponStats = 0;
+		Character.WeaponSentience = 0;
+		Character.ArmorName = "";
+		Character.ArmorStats = 0;
+		Character.ArmorSentience = 0;
+		Character.SecondaryWeaponName = "";
+		Character.SecondaryWeaponHealth = 0;
+		Character.SecondaryWeaponStats = 0;
+		Character.SecondaryWeaponSentience = 0;
+		Character.WeaponType = "";
+		Character.SecondaryWeaponType = "";
+		
+	}
 }
