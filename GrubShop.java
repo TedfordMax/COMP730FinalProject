00accf7195e85b28f9aa97d7f62d61fa397f8580
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-10 22:32:51 -0400

    Update GrubShop.java

--- GrubShop.java
+++ GrubShop.java
@@ -19,6 +19,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.ArmorItem("Scaley Armor", 2, -1);
+                        Character.Gold = Character.Gold - 100;
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -41,6 +43,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.Gold = Character.Gold - 200;
+                        Character.SecondaryWeaponItem("Sword of Barnacles", "Single", 1, 1, 1);
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -53,7 +57,7 @@ public class GrubShop {
                 }
                 break;
             case 3:
-                System.out.println("Gringo the Armsdealer: Ya want the Water Gun?");
+                System.out.println("Gringo the Armsdealer: Ya want the Water Pistol? It's a one-handed weapon that packs a punch and proves your smart enough to use a gun underwater!");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 300 coins.\n1) Buy for 300 coins\n2) Cancel purchase");
                 int purchase3 = sc.nextInt();
@@ -63,6 +67,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.SecondaryWeaponItem("Water Gun", "Single", 4, 3, 0);
+                        Character.Gold = Character.Gold - 300;
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -75,7 +81,7 @@ public class GrubShop {
                 }
                 break;
             case 4:
-                System.out.println("Gringo the Armsdealer: Ya want the Water Rifle?");
+                System.out.println("Gringo the Armsdealer: Ya want the Water Rifle? It's a two-handed weapon, you can't use an off-hand weapon with it but it's strong!");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 400 coins.\n1) Buy for 400 coins\n2) Cancel purchase");
                 int purchase4 = sc.nextInt();
@@ -85,6 +91,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.WeaponItem("Water Gun", "Two Handed", 5, 4);
+                        Character.Gold = Character.Gold - 400;
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -97,7 +105,7 @@ public class GrubShop {
                 }
                 break;
             case 5:
-                System.out.println("Gringo the Armsdealer: Ya want the Water Sword?");
+                System.out.println("Gringo the Armsdealer: Ya want the Water Sword? This is a dangerous weapon that only takes one hand to swing.");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 500 coins.\n1) Buy for 500 coins\n2) Cancel purchase");
                 int purchase5 = sc.nextInt();
@@ -107,6 +115,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.Gold = Character.Gold - 500;
+                        Character.WeaponItem("Water Sword", "Single", 8, 2);
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -119,7 +129,7 @@ public class GrubShop {
                 }
                 break;
             case 6:
-                System.out.println("Gringo the Armsdealer: Ya want the Whirlpool Activator?");
+                System.out.println("Gringo the Armsdealer: Ya want the Whirlpool Activator? It's an ancient weapon you can use in your off-hand!");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 600 coins.\n1) Buy for 600 coins\n2) Cancel purchase");
                 int purchase6 = sc.nextInt();
@@ -129,6 +139,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.Gold = Character.Gold - 600;
+                        Character.SecondaryWeaponItem("Whirlpool Activator", "Single", 1, 8, 3);
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -141,7 +153,7 @@ public class GrubShop {
                 }
                 break;
             case 7:
-                System.out.println("Gringo the Armsdealer: Ya want the Flame Thrower?");
+                System.out.println("Gringo the Armsdealer: Ya want the Flame Thrower? It's a two handed powerhouse, the most feared weapon I've got.");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 700 coins.\n1) Buy for 700 coins\n2) Cancel purchase");
                 int purchase7 = sc.nextInt();
@@ -151,6 +163,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.Gold = Character.Gold - 700;
+                        Character.WeaponItem("Flame Thrower", "Two Handed", 12, 0);
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
@@ -163,7 +177,7 @@ public class GrubShop {
                 }
                 break;
             case 8:
-                System.out.println("Gringo the Armsdealer: Ya want the Armor Piercing Blowfish?");
+                System.out.println("Gringo the Armsdealer: Ya want the Armor Piercing Blowfish? It's like a pistol, if you're smart you can really use this effectivley.");
                 try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                 System.out.println("Gringo the Armsdealer: I'll give it to ya for 800 coins.\n1) Buy for 800 coins\n2) Cancel purchase");
                 int purchase8 = sc.nextInt();
@@ -173,6 +187,8 @@ public class GrubShop {
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
                         System.out.println("Gringo the Armsdealer: Good luck out there in the open waters, kid.");
                         try{TimeUnit.SECONDS.sleep(3);}catch(InterruptedException e){e.printStackTrace();}
+                        Character.Gold = Character.Gold - 800;
+                        Character.WeaponItem("Armor Piercing Blowfish", "Single", 5, 5);
                         Storyboard.Crossroads(character, 1);
                         break;
                     case 2:
