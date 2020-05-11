56e4f5d55999581f94e94f538085561eb2f747a9
TedfordMax <42852936+TedfordMax@users.noreply.github.com>
2020-05-11 15:21:13 -0400

    Update Combat.java

--- Combat.java
+++ Combat.java
@@ -9,13 +9,11 @@ public class Combat  {
 		Character.getItemStats();
 		System.out.println("Type in the first of the three combat abilities you want to use this combat phase. Use 1, 2, or 3.");
 		String a = in.nextLine();
-		if (a.equals("1")) {System.out.println("You use " + Character.Attack1 + " dealing " + Character.Attack + " damage.");
-		Enemy.Health = Enemy.Health - Character.Attack;	}		
+		if (a.equals("1")) {Character.Attack1();}		
 		if (a.equals("2")) {Character.Attack2();}
 		if (a.equals("3"))  {Character.Attack3();}
 		int randomInt = rand.nextInt(3);
-		if (randomInt == 0) {Enemy.Attack1();
-			Character.Health = Character.Health - Enemy.Attack;}
+		if (randomInt == 0) {Enemy.Attack1();}
 		else if (randomInt == 1) {Enemy.Attack2();}
 		else if (randomInt == 2) {Enemy.Attack3();}
 		System.out.println("Your health = " + Character.Health);
@@ -23,13 +21,11 @@ public class Combat  {
 		
 		System.out.println("Type in the second of the three combat abilities you want to use this combat phase.");
 		String b = in.nextLine();
-		if (b.equals("1")) {System.out.println("You use" + Character.Attack1 + " dealing " + (Character.Attack + Character.WeaponStats) + " damage.");
-			Enemy.Health = Enemy.Health - Character.Attack;}
+		if (b.equals("1")) {Character.Attack1();}	
 		if (b.equals("2")) {Character.Attack2();}	
 		if (b.equals("3")) {Character.Attack3();}	
 		int randomInt2 = rand.nextInt(3);
-		if (randomInt2 == 0) {Enemy.Attack1();
-			Character.Health = Character.Health - Enemy.Attack;}
+		if (randomInt2 == 0) {Enemy.Attack1();}
 		else if (randomInt2 == 1) {Enemy.Attack2();}
 		else if (randomInt2 == 2) {Enemy.Attack3();}
 		System.out.println("Your health = " + Character.Health);
@@ -37,15 +33,14 @@ public class Combat  {
 
 		System.out.println("Type in the third of the three combat abilities you want to use this combat phase.");
 		String c = in.nextLine();
-		if (c.equals("1"))  {System.out.println("You use" + Character.Attack1 + " dealing " + Character.Attack + " damage.");
-			Enemy.Health = Enemy.Health - Character.Attack;}
+		if (c.equals("1")) {Character.Attack1();}	
 		if (c.equals("2")) {Character.Attack2();}
 		if (c.equals("3")) {Character.Attack3();}
 		int randomInt3 = rand.nextInt(3);
-		if (randomInt3 == 0) {Enemy.Attack1();
-			Character.Health = Character.Health - Enemy.Attack;}
+		if (randomInt3 == 0) {Enemy.Attack1();}
 		else if (randomInt3 == 1) {Enemy.Attack2();}
 		else if (randomInt3 == 2) {Enemy.Attack3();}
+		
 		System.out.println("Combat round over.");
 		System.out.println("Your health = " + Character.Health);
 		System.out.println("Enemy health = " + Enemy.Health);
